6614f8be4cb87a32af6ea253233bbda84151b050
Naomi Mugglestone <NJLM2708@outlook.com>
2022-08-24 19:49:00 +0100

    static messages placed

--- my-app/frontEnd/src/api/hobbyposts.js
+++ my-app/frontEnd/src/api/hobbyposts.js
@@ -5,4 +5,5 @@ export default axios.create({
     //this would need to be changed if it went live to the host URL
 })
 
-//npx json-server -p 4000 -w data/db.json  this should start up the json server that holds the information /posts etc  at this youtube https://www.youtube.com/watch?v=ZEKBDXGnD4s
+//npx json-server -p 4000 -w data/db.json   this should start up the json server that holds the
+// information /posts etc  at this youtube https://www.youtube.com/watch?v=ZEKBDXGnD4s
