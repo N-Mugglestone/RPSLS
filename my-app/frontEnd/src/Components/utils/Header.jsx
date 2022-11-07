4aba601cfa588a3479ad98f7ab0c02ae3efeb0d9
Naomi Mugglestone <NJLM2708@outlook.com>
2022-11-07 18:07:43 +0000

    css

--- my-app/frontEnd/src/Components/utils/Header.jsx
+++ my-app/frontEnd/src/Components/utils/Header.jsx
@@ -5,15 +5,47 @@ import '../Styles/header.css'
 
 
 const Header = () => {
+    // return (
+
+    //     <>
+    //         <nav>
+    //             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //                 <div className='container-fluid'>
+    //                     <Link className="navbar-brand" to='/'> </Link>
+    //                     <div className='collapse navbar-collapse' id="navbarNavAltMarkup"></div>
+    //                     <div className="navbar-nav">
+
+    //                         <h1> <Link className="nav-link" to="/"> <em>Home</em> </Link> </h1>
+
+    //                         <h1> <Link className="nav-link" to="/hobbies"> <em>Hobbies</em> </Link> </h1>
+    //                         <h1> <Link className="nav-link" to="/Wishlist"> <em>Wishlist</em> </Link> </h1>
+    //                         <h1> <Link className="nav-link" to="/random"> <em>Random</em> </Link> </h1>
+    //                         <h1> <Link className="nav-link" to="/bucketlist"> <em>Bucket List</em> </Link> </h1>
+    //                         <br />
+    //                         <h1><Link className="nav-Link" to="/AddHobbies"><em> Add Hobby</em> </Link> </h1>
+    //                         <h1><Link className="nav-Link" to="/addWishlist"><em> Add to Wishlist</em> </Link> </h1>
+    //                         <h1><Link className="nav-Link" to="/addRandom"><em> Add Random</em> </Link> </h1>
+    //                         <h1><Link className="nav-Link" to="/addBucketList"><em> Add to Bucket List</em> </Link> </h1>
+
+
+    //                     </div>
+    //                 </div>
+    //             </nav>
+    //         </nav>
+    //     </>
+
+    // )
+
+
     return (
 
         <>
             <nav>
-                <nav className="navbar navbar-expand-lg navbar-dark">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className='container-fluid'>
                         <Link className="navbar-brand" to='/'> </Link>
                         <div className='collapse navbar-collapse' id="navbarNavAltMarkup"></div>
-                        <div className="navbar-nav">
+                        <div className="navbar-nav mr-auto">
 
                             <h1> <Link className="nav-link" to="/"> <em>Home</em> </Link> </h1>
 
@@ -22,11 +54,18 @@ const Header = () => {
                             <h1> <Link className="nav-link" to="/random"> <em>Random</em> </Link> </h1>
                             <h1> <Link className="nav-link" to="/bucketlist"> <em>Bucket List</em> </Link> </h1>
                             <br />
-                            <h1><Link className="nav-Link" to="/AddHobbies"><em> Add Hobby</em> </Link> </h1>
-                            <h1><Link className="nav-Link" to="/addWishlist"><em> Add to Wishlist</em> </Link> </h1>
-                            <h1><Link className="nav-Link" to="/addRandom"><em> Add Random</em> </Link> </h1>
-                            <h1><Link className="nav-Link" to="/addBucketList"><em> Add to Bucket List</em> </Link> </h1>
 
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown">
+                                    Add a new item
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdoen.Item> <h1><Link className="nav-Link" to="/AddHobbies"><em> Add Hobby</em> </Link> </h1></Dropdoen.Item>
+                                    <Dropdown.Item> <h1><Link className="nav-Link" to="/addWishlist"><em> Add to Wishlist</em> </Link> </h1> </Dropdown.Item>
+                                    <Dropdown.Item> <h1><Link className="nav-Link" to="/addRandom"><em> Add Random</em> </Link> </h1></Dropdown.Item>
+                                    <Dropdown.Item> <h1><Link className="nav-Link" to="/addBucketList"><em> Add to Bucket List</em> </Link> </h1></Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
 
                         </div>
                     </div>
@@ -40,12 +79,51 @@ const Header = () => {
 export default Header;
 
 
+// play around with this to make it work for this
+
+// <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//     <a class="navbar-brand" href="#"> Home </a>
+//     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//     </button>
+
+//     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//         <ul class="navbar-nav mr-auto">
+//             <li class="nav-item active">
+//                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//             </li>
+//             <li class="nav-item">
+//                 <a class="nav-link" href="#">Link</a>
+//             </li>
+//             <li class="nav-item dropdown">
+//                 <a class="nav-link dropdown-toggle" href="#" id="navbarSections" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                     Sections
+//                 </a>
+//                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+//                     <a class="dropdown-item" href="#">Action</a>
+//                     <a class="dropdown-item" href="#">Another action</a>
+//                     <div class="dropdown-divider"></div>
+//                     <a class="dropdown-item" href="#">Something else here</a>
+//                 </div>
+//             </li>
+//             <li class="nav-item">
+//                 <a class="nav-link disabled" href="#">Disabled</a>
+//             </li>
+//         </ul>
+//     </div>
+// </nav>
 
 
 
 
-//                             <Link className="nav-link" to="/"> <h1> Home </h1> </Link>
-//                             <Link className="nav-link" to="/hobbies"> <h1> Hobbies </h1> </Link>
-//                             <Link className="nav-link" to="/wishlist"> <h1> Wishlist </h1> </Link>
-//                             <Link className="nav-link" to="/random"> <h1> Random </h1> </Link>
-//                             <Link className="nav-link" to="/tester"> <h1> tester </h1> </Link>
+
+
+
+
+// A search button and form  -may be useful later
+//         <form class="form-inline my-2 my-lg-0">
+//             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+//                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//         </form>
+
+
